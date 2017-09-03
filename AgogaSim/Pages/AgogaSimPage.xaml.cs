f93633f1b617f59bcc4e62f3c2e0d17a23d1f0a1
Leonardo Vitor da Silva <xportation@gmail.com>
2017-09-03 14:50:15 -0300

    Add tutorial alert. Change the logout icon to more icon and all actions can be done by popup.

--- AgogaSim/Pages/AgogaSimPage.xaml.cs
+++ AgogaSim/Pages/AgogaSimPage.xaml.cs
@@ -15,7 +15,32 @@ namespace AgogaSim
 			vm = new AgogaSimViewModel(credentialsService, restService);
 			BindingContext = vm;
 
-            ToolbarItems.Add(new ToolbarItem("", "ic_logout", () => App.Logout()));
+            ToolbarItems.Add(new ToolbarItem("", "ic_more", HandleMoreActionAsync));
+        }
+
+        async void HandleMoreActionAsync()
+        {
+            string pageAction = "Resumo do dia";
+            if (CurrentPage == Children[0])
+                pageAction = "Relatório de dias anteriores";
+
+            var action = await DisplayActionSheet(null, "Cancelar", null, "Logout", "Recarregar", pageAction);
+            switch (action) {
+                case "Logout":
+                    App.Logout();
+                    break;
+                case "Recarregar":
+                    vm.RefreshCommand.Execute(null);
+                    if (Device.RuntimePlatform == Device.iOS)
+                        ScrollResume.ScrollToAsync(0, -60, true);
+                    break;
+                case "Resumo do dia":
+                    CurrentPage = Children[0];
+                    break;
+                case "Relatório de dias anteriores":
+                    CurrentPage = Children[1];
+                    break;
+            }
         }
 
         void Handle_ItemTapped(object sender, Xamarin.Forms.ItemTappedEventArgs e)
